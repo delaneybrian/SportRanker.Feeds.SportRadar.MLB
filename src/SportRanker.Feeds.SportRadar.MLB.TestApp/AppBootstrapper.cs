c50a645aafc34477162d0c708e12de0eccd56c4a
= <=>
2018-12-21 15:07:33 +0000

    added data provider and test app

---
+++ src/SportRanker.Feeds.SportRadar.MLB.TestApp/AppBootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using SportRanker.Feeds.SportRadar.MLB.Application;
+using SportRanker.Feeds.SportRadar.MLB.Infrastructure;
+using SportRanker.Feeds.SportRadar.MLB.Interfaces;
+
+namespace SportRanker.Feeds.SportRadar.MLB.TestApp
+{
+    public class AppBootstrapper
+    {
+        public static IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterType<FeedProcessor>()
+                .As<IFeedProcessor>();
+
+            builder
+                .RegisterType<FeedConsumer>()
+                .As<IFeedConsumer>();
+
+            builder
+                .RegisterType<Publisher>()
+                .As<IPublisher>();
+
+            builder
+                .RegisterType<FixtureResultDeriver>()
+                .As<IFixtureResultDeriver>();
+
+            builder
+                .RegisterType<DataProvider>()
+                .As<IDataProvider>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/SportRanker.Feeds.SportRadar.MLB.TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using SportRanker.Feeds.SportRadar.MLB.Interfaces;
+
+namespace SportRanker.Feeds.SportRadar.MLB.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = AppBootstrapper.Bootstrap();
+
+            var feedProcessor = container.Resolve<IFeedProcessor>();
+
+            feedProcessor.ProcessHistoricalFixtures(3).Wait();
+        }
+    }
+}
